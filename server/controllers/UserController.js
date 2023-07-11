72ae2a4128f528d08f43eb8e6e1b4dfc42f58925
Kahalia Stanberry <kahalia.stanberry@gmail.com>
2023-07-10 21:44:44 -0400

    User table updated so that emails must be unique & error handling added

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -2,10 +2,17 @@ const db = require("../db");
 
 const UserController = {
   async getUsers(req, res, next) {
-    const queryText = "SELECT * FROM users;";
-    const allUsers = await db.query(queryText);
-    res.locals.allUsers = allUsers.rows;
-    return next();
+    const queryText = "SELECT * FROM users ORDER BY id ASC;";
+    const allUsers = await db.query(queryText, (err, res) => {
+      if (err) {
+        return next({
+          log: "Express error handler caught middleware error when getting ALL users",
+          message: { err: err },
+        });
+      }
+      res.locals.allUsers = allUsers.rows;
+      return next();
+    });
   },
   // async getUser(req, res, next) {
   //   const userEmail = req.params.email;
@@ -17,42 +24,69 @@ const UserController = {
   async getUserById(req, res, next) {
     const userId = parseInt(req.params.id);
     const queryText = "WHERE EXISTS (SELECT * FROM users WHERE id = $1);";
-    const user = await db.query(queryText, [userId]);
-    res.locals.userInfo = user.rows;
-    return next();
+    const user = await db.query(queryText, [userId], (err, res) => {
+      if (err) {
+        return next({
+          log: "Express error handler caught middleware error when getting user by id",
+          message: { err: err },
+        });
+      }
+      res.locals.userInfo = user.rows;
+      return next();
+    });
   },
   async createUser(req, res, next) {
     const { user_email, user_password, first_name, last_name } = req.body;
     const queryText =
       "INSERT INTO users (id, user_email, user_password, first_name, last_name) VALUES(DEFAULT, $1, $2,$3, $4)";
-    const user = await db.query(queryText, [
-      user_email,
-      user_password,
-      first_name,
-      last_name,
-    ]);
-    res.locals.newUser = user.rows;
-    return next();
+    const user = await db.query(
+      queryText,
+      [user_email, user_password, first_name, last_name],
+      (err, res) => {
+        if (err) {
+          return next({
+            log: "Express error handler caught middleware error when creating user",
+            message: { err: err },
+          });
+        }
+        res.locals.newUser = user.rows;
+        return next();
+      }
+    );
   },
   async updateUser(req, res, next) {
     const id = parseInt(req.params.id);
     const { user_email, user_password } = req.body;
     const queryText =
       "UPDATE users SET user_email = $1, user_password = $2 WHERE id = $3";
-    const updatedUser = await db.query(queryText, [
-      user_email,
-      user_password,
-      id,
-    ]);
+    const updatedUser = await db.query(
+      queryText,
+      [user_email, user_password, id],
+      (err, res) => {
+        if (err) {
+          return next({
+            log: "Express error handler caught middleware error when updating user",
+            message: { err: err },
+          });
+        }
 
-    return next();
+        return next();
+      }
+    );
   },
   async deleteUser(req, res, next) {
     const id = parseInt(req.params.id);
     const queryText = "DELETE FROM users WHERE id = $1";
-    const updatedUser = await db.query(queryText, [id]);
+    const updatedUser = await db.query(queryText, [id], (err, res) => {
+      if (err) {
+        return next({
+          log: "Express error handler caught middleware error when updating user",
+          message: { err: err },
+        });
+      }
 
-    return next();
+      return next();
+    });
   },
 };
 
--- server/server.js
+++ server/server.js
@@ -32,6 +32,18 @@ if (process.env.NODE_ENV === "development") {
 
 app.use("/api/users", userRoutes);
 
+// Global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: "Express error handler caught unknown middleware error",
+    status: 400,
+    message: { err: "An error occurred" },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
 app.listen(PORT, () => {
   console.log("Server started on port " + PORT);
 });
