b8cb3c5f269315efe552589c967f216dfa05c2a9
Lisa Rodrigues <lisa.rodrigues8@outlook.com>
2023-07-10 18:17:56 -0700

    Added break lines to Signup.jsx for formatting

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -37,19 +37,23 @@ const Signup = () => {
             {/*First Name Input*/}
             <label htmlFor="firstName" className="signup-label" placeholder="First Name">First Name</label>
             <input type="text" id="signupFName" name="firstName" className="signup-input"/>
+            <br /><br />
 
             {/*Last Name Input*/}
             <label htmlFor="lastName" className="signup-label" placeholder="Last Name">Last Name</label>
             <input type="text" id="signupLName" name="lastName" className="signup-input"/>
+            <br /><br />
 
             {/*Email Input*/}
             <label htmlFor="email" className="signup-label" placeholder="Email">Email</label>
             <input type="text" id="signupEmail" name="email" className="signup-input"/>
+            <br /><br />
 
             {/*Password Input*/}
             <label htmlFor="password" className="signup-label" placeholder="Password">Password</label>
             <input type="text" id="signupPassword" name="password" className="signup-input"/>
-
+            <br /><br />
+            
             {/*Signup Buttons*/}
             <button className="signup-button" onClick={() => createUser(document.querySelector('#signupFName').value, document.querySelector('#signupLName').value, document.querySelector('#signupEmail').value, document.querySelector('#signupPassword').value)}>Sign Up</button>
 
